6095b357cb4f4d6f521688c3e993486343b2444f
Filiph Schaaf <filiph.ps@gmail.com>
2025-04-03 14:20:50 +0200

    Add setting for read-only filesystems

--- solr-backup/entrypoint.sh
+++ solr-backup/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-BACKUP_COMMAND="/var/solr-backup/backup-solr.sh"
+if [[ -z "${READ_ONLY}" ]]; then
+    BACKUP_COMMAND="/var/solr-backup/backup-solr.sh"
 
-if [[ -z "${CRON_SCHEDULE}" ]]; then
-    CRON_SCHEDULE='0 0 * * * $BACKUP_COMMAND'
+    if [[ -z "${CRON_SCHEDULE}" ]]; then
+        CRON_SCHEDULE='0 0 * * * $BACKUP_COMMAND'
+    fi
+
+    CRON_SCHEDULE="${CRON_SCHEDULE//\$BACKUP_COMMAND/$BACKUP_COMMAND}"
+    echo "$CRON_SCHEDULE" > "/var/solr-backup/crontab"
 fi
-
-CRON_SCHEDULE="${CRON_SCHEDULE//\$BACKUP_COMMAND/$BACKUP_COMMAND}"
-echo "$CRON_SCHEDULE" > "/var/solr-backup/crontab"
 
 exec /usr/local/bin/supercronic -passthrough-logs -quiet "/var/solr-backup/crontab"
